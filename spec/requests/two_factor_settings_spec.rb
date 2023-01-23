555bb9400e9144c2818120405ff94ad388267a21
Wendreo Teixeira <wendreo.pvh@gmail.com>
2023-01-23 17:43:11 -0300

    added 2FA edit and new and qr code helper

---
+++ spec/requests/two_factor_settings_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "TwoFactorSettings", type: :request do
+  describe "GET /new" do
+    it "returns http success" do
+      get "/two_factor_settings/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/two_factor_settings/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
