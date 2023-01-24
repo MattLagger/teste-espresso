ea9b5c255cbf87b87e307ad73786450d7724a76f
Wendreo Teixeira <wendreo.pvh@gmail.com>
2023-01-24 16:13:51 -0300

    tested model and helpers

---
+++ spec/factories/user.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { 'Abc123!!defghaijklmnop%' }
+
+    trait :invalid_email do
+      email { 'false email here' }
+    end
+
+    trait :password_missing_uppercase do
+      password { 'abc123!!defghaijklmnop%' }
+    end
+
+    trait :password_missing_lowercase do
+      password { 'ABC123!!DEFGHAIJKLMNOP%' }
+    end
+
+    trait :with_otp do
+      otp_required_for_login { true }
+
+      after(:build) do |user, _evaluator|
+        user.otp_secret = User.generate_otp_secret
+        user.otp_plain_backup_codes = user.generate_otp_backup_codes!
+      end
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,57 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe(User, type: :model) do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'User register validation' do
+    it 'user password is valid' do
+      user = create(:user)
+      expect(user).to(be_valid)
+    end
+
+    it 'user email is invalid' do
+      user = build(:user, :invalid_email)
+      expect(user).not_to(be_valid)
+    end
+
+    it 'user password is missing uppercase' do
+      user = build(:user, :password_missing_uppercase)
+      user.validate
+      expect(user.errors.messages[:password][0]).to(eq(' must contain at least one uppercase letter'))
+    end
+
+    it 'user password is missing lowercase' do
+      user = build(:user, :password_missing_lowercase)
+      user.validate
+      expect(user.errors.messages[:password][0]).to(eq(' must contain at least one lowercase letter'))
+    end
+  end
+
+  context 'User two factor test' do
+    before { @user = create(:user) }
+
+    it 'generates new 2FA secret' do
+      @user.generate_two_factor_secret_if_missing!
+      expect(@user.otp_secret?).to(be(true))
+    end
+
+    it 'enable 2FA' do
+      @user.enable_two_factor!
+      expect(@user.otp_required_for_login).to(be(true))
+    end
+
+    it '2FA qr_code_uri' do
+      qr_code_uri = @user.two_factor_qr_code_uri
+      expect(qr_code_uri.present?).to(eq(true))
+    end
+
+    it 'generate backup codes' do
+      @user.generate_otp_backup_codes!
+      expect(@user.two_factor_backup_codes_generated?).to(be(true))
+    end
+
+    it 'disable 2FA secret' do
+      @user.disable_two_factor!
+      expect(@user.otp_secret?).to(be(false))
+    end
+  end
 end
+
